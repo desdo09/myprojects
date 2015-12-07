43f08bd97a380cf96cdc34fd51f4db55b1efe6ec
desdo09 <desdo09@hotmail.com>
2015-12-07 14:48:20 +0200

    c

--- BE/Ordered-Dish.cs
+++ BE/Ordered-Dish.cs
@@ -8,10 +8,38 @@ namespace BE
 {
     public class Ordered_Dish
     {
+        int _Ordered_DishId;
         int _OrderId;
         int _DishId;
         int _DishAmount;
 
+        public Ordered_Dish(int _Ordered_DishId, int _OrderId, int _DishId, int _DishAmount)
+        {
+            this._Ordered_DishId = _Ordered_DishId;
+            this._OrderId = _OrderId;
+            this._DishId = _DishId;
+            this._DishAmount = _DishAmount;
+        }
+        public void copy(Ordered_Dish a)
+        {
+            this._Ordered_DishId = a._Ordered_DishId;
+            this._OrderId = a._OrderId;
+            this._DishId = a._DishId;
+            this._DishAmount = a._DishAmount;
+        }
+
+        public int Ordered_DishId
+        {
+            get
+            {
+                return _Ordered_DishId;
+            }
+
+            set
+            {
+                _Ordered_DishId = value;
+            }
+        }
         public int OrderId
         {
             get
@@ -50,5 +78,6 @@ namespace BE
                 _DishAmount = value;
             }
         }
+
     }
 }
