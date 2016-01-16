2f606b83a45bf238422a6ce4e09a128b055a3d0e
desdo09 <desdo09@hotmail.com>
2016-01-16 22:06:25 +0200

    DAL Xml added

--- DAL/DAL_xml.cs
+++ DAL/DAL_xml.cs
@@ -4,24 +4,124 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BE;
+using System.Xml.Linq;
+using System.IO;
 
 namespace DAL
 {
     internal class DAL_xml : IDAL
     {
-        ClientToXmlClass StudentXml = new ClientToXmlClass();
+
+
+        XElement Root;
+        string OrderPath = @"xml/Order.xml";
+        string ClientPath = @"xml/Clients.xml";
+        string BranchPath = @"xml/Branch.xml";
+        string DishPath = @"xml/Dish.xml";
+        string OrderDishPath = @"xml/OrderDish.xml";
+
+
+        public DAL_xml()
+        {
+            if (!Directory.Exists(@"xml"))
+                Directory.CreateDirectory(@"xml");
+
+
+            if (!File.Exists(OrderPath))
+            {
+                Root = new XElement("Orders");
+                Root.Save(OrderPath);
+            }
+            else
+                LoadData(OrderPath);
+
+
+
+            if (!File.Exists(OrderPath))
+            {
+                Root = new XElement("Clients");
+                Root.Save(OrderPath);
+            }
+            else
+                LoadData(OrderPath);
+
+            if (!File.Exists(OrderPath))
+            {
+                Root = new XElement("Branches");
+                Root.Save(OrderPath);
+            }
+            else
+                LoadData(OrderPath);
+
+            if (!File.Exists(OrderPath))
+            {
+                Root = new XElement("Dishes");
+                Root.Save(OrderPath);
+            }
+            else
+                LoadData(OrderPath);
+
+            if (!File.Exists(OrderPath))
+            {
+                Root = new XElement("OrderDishes");
+                Root.Save(OrderPath);
+            }
+            else
+                LoadData(OrderPath);
+        }
+
+        private void LoadData(string Path)
+        {
+            try
+            {
+                Root = XElement.Load(Path);
+            }
+            catch
+            {
+                throw new Exception("File upload problem");
+            }
+        }
+
+
+
+
+
+
+
         public void AddBranch(Branch add)
         {
-            throw new NotImplementedException();
+
+            LoadData(BranchPath);
+
+            XElement id = new XElement("id", add.BranchId);
+            XElement Name = new XElement("Name", add.BranchName);
+            XElement Address = new XElement("Address", add.BranchAddres);
+            XElement Phone = new XElement("Phone", add.BranchPhone);
+            XElement BranchManager = new XElement("Card", add.BranchManager);
+            XElement BranchWorkers = new XElement("Card", add.BranchWorkers);
+            XElement NumOfDeliveryPerson = new XElement("Card", add.NumOfDeliveryPerson);
+            XElement BranchHashgacha = new XElement("Card", add.BranchHashgacha);
+
+            Root.Add(new XElement("Clients", id, Name, Address, Phone, BranchManager, BranchWorkers, NumOfDeliveryPerson, BranchHashgacha));
+            Root.Save(BranchPath);
         }
 
         public void AddClient(Client add)
         {
-            
-            StudentXml.AddClient(add);
-         
+
+            XElement id = new XElement("id", add.ClientId);
+            XElement Name = new XElement("Name", add.ClientName);
+            XElement Age = new XElement("Age", add.ClientAge);
+            XElement Address = new XElement("Address", add.ClientAddress);
+            XElement Phone = new XElement("Phone", add.ClientPhone);
+            XElement Card = new XElement("Card", add.ClientCard);
+            XElement Client = new XElement("Client_Details", Name, Age, Address, Phone);
+
+            Root.Add(new XElement("Clients", id, Client, Card));
+            Root.Save(ClientPath);
+
         }
-     
+
 
 
         public void AddDish(Dish add)
